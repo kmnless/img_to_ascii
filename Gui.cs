3037a3526a2602a7d9598e36920a7c2a351ced98
unknown <baga092005@gmail.com>
2023-02-08 20:32:39 +0100

    BOBRI SRALI NA POL A POTOM ELI

---
+++ Gui.cs
@@ -0,0 +1,255 @@
+﻿using System.Runtime.InteropServices;
+
+namespace img_to_ascii
+{
+    class Gui
+    {
+        public string? path;
+
+        public RENDER_MODE renderMode = RENDER_MODE.MONOCHROME;
+
+        public bool isAsciiTableLong = false;
+
+        public int imageWidth = 550;
+
+        public void Start()
+        {
+            byte choice = 1;
+            ConsoleKey key;
+            byte keyCount = 3;
+            bool isWorking = true;
+
+            while (isWorking)
+            {
+                Console.CursorVisible = false;
+                Console.WriteLine("\t\tImage to ASCII");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                if (choice == 1)
+                    SelectButton();
+                Console.Write("\tLoad image");
+                UnselectButton();
+                if (choice == 2)
+                    SelectButton();
+                Console.Write("\t\tSettings");
+                UnselectButton();
+                if (choice == 3)
+                    SelectButton();
+                Console.Write("\t\tExit");
+                UnselectButton();
+                Console.WriteLine();
+
+                key = Console.ReadKey().Key;
+
+                if (key == ConsoleKey.RightArrow)
+                {
+                    if (choice >= keyCount)
+                    {
+                        choice = 1;
+                    }
+                    else
+                    {
+                        choice++;
+                    }
+                }
+                else if (key == ConsoleKey.LeftArrow)
+                {
+                    if (choice <= 1)
+                    {
+                        choice = keyCount;
+                    }
+                    else
+                    {
+                        choice--;
+                    }
+                }
+                else if (key == ConsoleKey.Enter)
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Clear();
+                            LoadImage();
+                            isWorking = false;
+                            break;
+                        case 2:
+                            Console.Clear();
+                            ShowSettings();
+                            Console.Clear();
+                            break;
+                        case 3:
+                            isWorking = false;
+                            Console.Clear();
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                Console.Clear();
+            }
+        }
+
+        private void LoadImage()
+        {
+            bool success = false;
+            Console.CursorVisible = true;
+            while (!success)
+            {
+                Console.WriteLine("Input file path");
+                path = Console.ReadLine();
+                if (path == null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error");
+                }
+                else
+                {
+                    success = true;
+                    Console.Clear();
+                    Console.CursorVisible = false;
+                }
+            }
+        }
+
+        private void ShowSettings()
+        {
+            bool isWorking = true;
+            byte choice = 1;
+            byte keyCount = 4;
+            ConsoleKey key;
+
+            while (isWorking)
+            {
+                Console.WriteLine();
+                if (choice == 1)
+                    SelectButton();
+                Console.Write("Render type:");
+                UnselectButton();
+                if (renderMode == RENDER_MODE.COLORED)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.WriteLine("  Colored");
+                }
+                else if (renderMode == RENDER_MODE.MONOCHROME)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine("  Monochrome");
+                }
+                else if (renderMode == RENDER_MODE.BLACK_WHITE)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("  Black and white");
+                }
+                UnselectButton();
+
+                if (choice == 2)
+                    SelectButton();
+                Console.Write("ASCII table select:");
+                UnselectButton();
+                if (isAsciiTableLong)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("  Long");
+                }
+                else if (!isAsciiTableLong)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("  Short");
+                }
+                UnselectButton();
+
+                if (choice == 3)
+                    SelectButton();
+                Console.Write("Image width(length is proportional)");
+                UnselectButton();
+                if (imageWidth <= 400 || imageWidth >= 750)
+                    Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"  {imageWidth}");
+                UnselectButton();
+
+                if (choice == 4)
+                    SelectButton();
+                Console.Write("Exit");
+                UnselectButton();
+
+
+                key = Console.ReadKey().Key;
+
+                if (key == ConsoleKey.DownArrow)
+                {
+                    if (choice >= keyCount)
+                        choice = 1;
+                    else
+                        choice++;
+                }
+                else if (key == ConsoleKey.UpArrow)
+                {
+                    if (choice <= 1)
+                        choice = keyCount;
+                    else
+                        choice--;
+                }
+                else if (key == ConsoleKey.Enter)
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Clear();
+                            if ((int)renderMode < 2)
+                                renderMode++;
+                            else
+                                renderMode = RENDER_MODE.BLACK_WHITE;
+                            break;
+                        case 2:
+                            Console.Clear();
+                            if (isAsciiTableLong)
+                                isAsciiTableLong = false;
+                            else
+                                isAsciiTableLong = true;
+                            break;
+                        case 3:
+                            key = ConsoleKey.A;
+                            while (key != ConsoleKey.Enter)
+                            {
+                                Console.SetCursorPosition(37, 3);
+                                if (imageWidth <= 400 || imageWidth >= 750)
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine(imageWidth);
+                                UnselectButton();
+                                key = Console.ReadKey().Key;
+                                if (key == ConsoleKey.UpArrow)
+                                    imageWidth += 5;
+                                else if (key == ConsoleKey.DownArrow)
+                                    imageWidth -= 5;
+                            }
+                            break;
+                        case 4:
+                            Console.Clear();
+                            isWorking = false;
+
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                Console.Clear();
+            }
+        }
+
+        private void SelectButton()
+        {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+
+        private void UnselectButton()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Drawing;
+using img_to_ascii;
+
+Console.Title = "Image to ASCII";
+
+Gui gui = new Gui();
+
+RenderHandler.renderMode = RENDER_MODE.BLACK_WHITE;
+bool success = false;
+
+Image? image = null;
+
+// Trying get image
+while (!success)
+{
+    gui.Start();
+    RenderHandler.renderMode = gui.renderMode;
+    try
+    {
+        image = Image.FromFile(gui.path);
+        success= true;
+    }
+    catch
+    {
+        
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Wrong path!");
+        Console.ForegroundColor = ConsoleColor.Gray;
+        success = false;
+    }
+}
+
+// setting windowsize adjusted to image width
+int asciiImageWidth = gui.imageWidth;
+Console.SetWindowSize(Convert.ToInt32(asciiImageWidth/4.56), Console.LargestWindowHeight-10);
+
+Bitmap imageBitmap = new Bitmap(image);
+const int cellSize = 12;
+
+double transmotionKoef = asciiImageWidth / Convert.ToDouble(image.Width);
+imageBitmap = RenderHandler.ResizeImage(image, Convert.ToInt32(asciiImageWidth), Convert.ToInt32(imageBitmap.Height*transmotionKoef));
+// make image wider
+imageBitmap = RenderHandler.ResizeImage(image, cellSize * (imageBitmap.Width * 175 / 100 / cellSize) * 15/10, cellSize * (imageBitmap.Height / cellSize)*15/10);
+image = imageBitmap as Image;
+
+// symbol counters
+int bricksX = image.Width / cellSize;
+int bricksY = image.Height / cellSize;
+
+char[,] charArr = new char[bricksX, bricksY];
+
+const string asciiTablLeLong = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+const string asciiTableShort = "@&#0Oo*^.   ";
+string asciiTable;
+
+if(gui.isAsciiTableLong)
+    asciiTable = asciiTablLeLong;
+else
+    asciiTable = asciiTableShort;
+
+//asciiTable = RenderHandler.Reverse(asciiTable);
+
+// fill symbol array with chars
+for (int i = 0; i < bricksX; ++i)
+{
+    for (int j = 0; j < bricksY; ++j)
+    {
+        charArr[i, j] = asciiTable[Convert.ToInt32(RenderHandler.GetAverageBrightnessForCell(imageBitmap, cellSize, i, j) * (asciiTable.Length - 1))];
+    }
+}
+
+RenderHandler.WriteAsAscii(charArr, bricksX, bricksY, imageBitmap, cellSize);
