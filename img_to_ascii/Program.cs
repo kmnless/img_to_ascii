11e8283429c0a1e4d8d4158fb31a32a94a1836b0
Babk_ut38@student.itstep.org <Babk_ut38@student.itstep.org>
2023-02-08 03:05:13 +0200

    rabotaet

--- img_to_ascii/Program.cs
+++ img_to_ascii/Program.cs
@@ -2,43 +2,168 @@
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 
-Image image = Image.FromFile("D:\\testimages\\otval.jpg");
-Bitmap image_bitmap = new Bitmap(image);
-int cell_size = 31;
+bool withColor = true;
 
-while(image.Width%cell_size!=0 && image.Height %cell_size!= 0)
+Image image = Image.FromFile("D:\\testimages\\hitler2.png");
+Bitmap imageBitmap = new Bitmap(image);
+int cellSize = 3;
+
+// Resize image for cells
+while (image.Width % cellSize != 0 && image.Height % cellSize != 0)
 {
-    Console.WriteLine(cell_size);
-    cell_size++;
-    if(cell_size * (image.Height / cell_size) == 0 || cell_size * (image.Width / cell_size) == 0)
+    cellSize++;
+    if (cellSize * (image.Height / cellSize) == 0 || cellSize * (image.Width / cellSize) == 0)
     {
-        Console.WriteLine("Image is in bad res");
+        //Console.WriteLine("Not resized for cell aspect ratio");
         break;
     }
-    if (cell_size >= 50)
+    if (cellSize >= 30)
     {
-        image_bitmap = ResizeImage(image, cell_size * (image.Width / cell_size), cell_size * (image.Height / cell_size));
-        Console.WriteLine("Success");
+        imageBitmap = ResizeImage(image, cellSize * (image.Width / cellSize), cellSize * (image.Height / cellSize));
+        //Console.WriteLine("Resized");
         break;
     }
 }
 
-int bricksX = image.Width / cell_size;
-int bricksY = image.Height / cell_size;
-Image newImage = image_bitmap as Image;
-newImage.Save("D:\\testimages\\resized\\oisya1.png");
-int GetAverageBrightness(Bitmap imageBitmap, int cellSize, int CellNumX, int CellNumY) 
+// make image wider
+imageBitmap = ResizeImage(image, cellSize * (image.Width * 175 / 100 / cellSize) * 15/10, cellSize * (image.Height / cellSize)*15/10);
+image = imageBitmap as Image;
+
+// symbol counters
+int bricksX = image.Width / cellSize;
+int bricksY = image.Height / cellSize;
+
+char[,] charArr = new char[bricksX, bricksY];
+string asciiTable = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+//string asciiTable = "@&#0Oo*^.  ";
+
+asciiTable = Reverse(asciiTable);
+
+// fill symbol array with chars
+for (int i = 0; i < bricksX; ++i)
 {
-    int summBrightness = 0;
-    for(int x = 0; x < cellSize; ++x)
+    for (int j = 0; j < bricksY; ++j)
+    {
+        charArr[i, j] = asciiTable[Convert.ToInt32(GetAverageBrightnessForCell(imageBitmap, cellSize, i, j) * (asciiTable.Length - 1))];
+    }
+}
+
+WriteAsAscii(charArr, bricksX, bricksY, imageBitmap, cellSize);
+
+
+//Console.ForegroundColor = (ConsoleColor)imageBitmap.GetPixel(0, 0).ToKnownColor();
+
+
+string Reverse(string a)
+{
+    string toReturn = "";
+    for (int i = 0; i < a.Length; ++i)
+    {
+        toReturn += a[a.Length - i - 1];
+    }
+    return toReturn;
+}
+
+void WriteAsAscii(char[,] arr, int sizeX, int sizeY, Bitmap? imageBitmap = null, int cellSize = 0)
+{
+    ConsoleColor color = ConsoleColor.Gray;
+    for (int x = 0; x < sizeY; ++x)
+    {
+        for (int y = 0; y < sizeX; ++y)
+        {
+            if (imageBitmap != null && cellSize != 0 && withColor)
+            {
+                color = GetAverageColorForCell(imageBitmap, cellSize, y, x);
+            }
+            Console.ForegroundColor = color;
+            Console.Write(arr[y, x]);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        Console.WriteLine();
+    }
+}
+
+float GetAverageBrightnessForCell(Bitmap imageBitmap, int cellSize, int CellNumX, int CellNumY)
+{
+    float summBrightness = 0;
+    float pixelBrightness = 0;
+
+    for (int x = 0; x < cellSize; ++x)
     {
         for (int y = 0; y < cellSize; ++y)
         {
-            imageBitmap.GetPixel(x+CellNumX)
+            pixelBrightness = imageBitmap.GetPixel(x + CellNumX * cellSize, y + CellNumY * cellSize).GetBrightness();
+            summBrightness += pixelBrightness;
         }
     }
+    return summBrightness / cellSize / cellSize;
 }
-static Bitmap ResizeImage(Image image, int new_width, int new_height)
+
+ConsoleColor GetAverageColorForCell(Bitmap imageBitmap, int cellSize, int CellNumX, int CellNumY)
+{
+    int red = 0;
+    int green = 0;
+    int blue = 0;
+    for (int x = 0; x < cellSize; ++x)
+    {
+        for (int y = 0; y < cellSize; ++y)
+        {
+            red += imageBitmap.GetPixel(x + CellNumX * cellSize, y + CellNumY * cellSize).R;
+            green += imageBitmap.GetPixel(x + CellNumX * cellSize, y + CellNumY * cellSize).G;
+            blue += imageBitmap.GetPixel(x + CellNumX * cellSize, y + CellNumY * cellSize).B;
+        }
+    }
+    red = red / cellSize / cellSize;
+    green = green / cellSize / cellSize;
+    blue = blue / cellSize / cellSize;
+    byte r = Convert.ToByte(red / (cellSize * cellSize));
+    byte g = Convert.ToByte(green / (cellSize * cellSize));
+    byte b = Convert.ToByte(blue / (cellSize * cellSize));
+    Color color = Color.FromArgb(red, green, blue); 
+
+    return GetConsoleColor(color);
+}
+
+ConsoleColor GetConsoleColor(Color color)
+{
+    if (color.GetSaturation() < 0.5)
+    {
+        // we have a grayish color
+        switch ((int)(color.GetBrightness() * 3.5))
+        {
+            case 0: return ConsoleColor.Black;
+            case 1: return ConsoleColor.DarkGray;
+            case 2: return ConsoleColor.Gray;
+            default: return ConsoleColor.White;
+        }
+    }
+    int hue = (int)Math.Round(color.GetHue() / 60, MidpointRounding.AwayFromZero);
+    if (color.GetBrightness() < 0.4)
+    {
+        // dark color
+        switch (hue)
+        {
+            case 1: return ConsoleColor.DarkYellow;
+            case 2: return ConsoleColor.DarkGreen;
+            case 3: return ConsoleColor.DarkCyan;
+            case 4: return ConsoleColor.DarkBlue;
+            case 5: return ConsoleColor.DarkMagenta;
+            default: return ConsoleColor.DarkRed;
+        }
+    }
+    // bright color
+    switch (hue)
+    {
+        case 1: return ConsoleColor.Yellow;
+        case 2: return ConsoleColor.Green;
+        case 3: return ConsoleColor.Cyan;
+        case 4: return ConsoleColor.Blue;
+        case 5: return ConsoleColor.Magenta;
+        default: return ConsoleColor.Red;
+    }
+}
+
+Bitmap ResizeImage(Image image, int new_width, int new_height)
 {
     Rectangle destRect = new Rectangle(0, 0, new_width, new_height);
     Bitmap? destImage = new Bitmap(new_width, new_height);
